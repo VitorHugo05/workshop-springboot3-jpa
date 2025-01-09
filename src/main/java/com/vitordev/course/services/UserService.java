424e3c093ef9e0079896c80c3fc47d1fda6dce0a
VitorHugo05 <vitorhugo.pozzi@gmail.com>
2025-01-09 17:43:50 -0300

    Exception handling - update

--- src/main/java/com/vitordev/course/services/UserService.java
+++ src/main/java/com/vitordev/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.vitordev.course.entities.User;
 import com.vitordev.course.repositories.UserRepository;
 import com.vitordev.course.services.exceptions.DatabaseException;
 import com.vitordev.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
